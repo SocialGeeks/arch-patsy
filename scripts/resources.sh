e42add3b94cf11cd7632d081e1c17110f87d08ab
Ian Laird <irlaird@gmail.com>
2015-09-13 02:43:17 -0600

    Added default VIM setup and supporting fonts

--- scripts/resources.sh
+++ scripts/resources.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 # Install terminal
-pacman -S --noconfirm rxvt-unicode
+sudo pacman -S --noconfirm rxvt-unicode
 
 # Install global resources
 sudo mkdir /usr/share/fonts
-sudo mv /vagrant/DejaVu\ Sans\ Mono\ for\ Powerline.ttf /usr/share/fonts/
+sudo mv /vagrant/*.ttf /usr/share/fonts/
 sudo mv /vagrant/motd /etc/
 sudo mv /vagrant/sgprompt.sh /etc/profile.d/
 sudo fc-cache /usr/share/fonts/
 sudo mv /vagrant/user.bashrc /etc/skel/.bashrc
 
 # Install user resources
-mv /vagrant/README.txt /home/vagrant/
-mv /vagrant/Xdefaults /home/vagrant/.Xdefaults
-mv /vagrant/user.bashrc /home/vagrant/.bashrc
+sudo -u vagrant mv /vagrant/README.txt /home/vagrant/
+sudo -u vagrant mv /vagrant/Xdefaults /home/vagrant/.Xdefaults
+sudo -u vagrant mv /vagrant/user.bashrc /home/vagrant/.bashrc
+sudo -u vagrant mv /vagrant/vimrc /home/vagrant/.vimrc
 
 # Install root resources
 sudo mv /vagrant/root.bashrc /root/.bashrc
